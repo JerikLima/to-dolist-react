2ccf1ebd854c21cb35603023df33da3004341629
Erik Lima <jonherik123@gmail.com>
2024-11-03 23:08:25 -0300

    Feat/Line-ThroughOnClick

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -3,10 +3,15 @@ import { ChevronRightIcon } from "lucide-react";
 function Tasks(props) {
   return (
     <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
-      {props.tasks.map((tasks) => (
-        <li key={tasks.id} className="flex">
-          <button className="bg-slate-400 text-white p-2 rounded-md w-full">
-            {tasks.title}
+      {props.tasks.map((task) => (
+        <li key={task.id} className="flex">
+          <button
+            onClick={() => props.onTaskClick(task.id)}
+            className={`bg-slate-400 text-white p-2 rounded-md w-full ${
+              task.isCompleted && "line-through"
+            }`}
+          >
+            {task.title}
           </button>
           <button className="bg-slate-400 p-2 rounded-md shadow text-white ml-2">
             <ChevronRightIcon />
